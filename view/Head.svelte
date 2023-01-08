959a8fb2375ba86ebd8d99e60a254dc0dafe2a89
Warren <warrenb95@live.co.uk>
2023-01-08 11:38:47 +0000

    revert dockerfile changes

--- view/Head.svelte
+++ view/Head.svelte
@@ -2,7 +2,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>warrenb95</title>
+    <title>😄 warrenb95</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
