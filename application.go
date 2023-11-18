e85ccb31e587f0e7bc95452b3222183f0161910b
Warren <warrenb95@live.co.uk>
2023-11-18 13:17:32 +0000

    host on port 80

--- application.go
+++ application.go
@@ -15,7 +15,7 @@ func main() {
 	// AWS Elastic Beanstalk runs off port 5000.
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "5000"
+		port = "80"
 	}
 
 	// Save the logs here for AWS Elastic Beanstalk.
